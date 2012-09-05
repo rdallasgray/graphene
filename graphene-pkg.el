3e5789a3e304e5d228013209e59343512d13a463
Robert Dallas Gray <mail@robertdallasgray.com>
2012-09-05 19:53:33 +0100

    Added graphene-pkg with all dependencies.

---
+++ graphene-pkg.el
@@ -0,0 +1,10 @@
+(define-package "graphene" "0.0.1"
+  "An opinionated environment for users migrating from Textmate, Sublime Text, etc."
+  '((auto-complete "20120829.26")
+    (autopair "20120818.1646")
+    (smart-tab "20120409.940")
+    (multi-web-mode "20110821.1317")
+    (smex "20120831.2023")
+    (expand-region "20120823.1553")
+    (color-theme-solarized "20120829.2248")
+    (project-mode "1.0")))
