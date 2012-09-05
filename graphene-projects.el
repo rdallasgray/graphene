3bf7edc33366a56a9fcc184cfcc0514ad6c796df
Robert Dallas Gray <mail@robertdallasgray.com>
2012-09-05 19:21:16 +0100

    Using separate folders for projects, desktops saving correctly.

--- graphene-projects.el
+++ graphene-projects.el
@@ -6,15 +6,14 @@
 
 (require 'graphene-helper-functions)
 
-(defvar graphene-project-folder (concat user-emacs-directory "projects/"))
+(defvar graphene-projects-folder (concat user-emacs-directory "projects/"))
 
-(unless (file-exists-p graphene-project-folder)
-  (make-directory graphene-project-folder))
+(unless (file-exists-p graphene-projects-folder)
+  (make-directory graphene-projects-folder))
 
 ;; Enable project-mode.
 (require 'project-mode)
 (project-mode t)
-(setq project-proj-files-dir graphene-project-folder)
 
 ;; Define hooks.
 (defvar graphene-project-before-open-hook)
@@ -22,7 +21,24 @@
 (defvar graphene-project-after-save-hook)
 
 ;; Define a variable to expose the project's root directory.
-(defvar graphene-project-root-dir)
+(defvar graphene-project-root)
+
+;; Project folder for the .proj and .desktop files etc.
+(defvar graphene-project-folder)
+
+;; Set graphene-project-root, set default-directory, update speedbar
+(defun graphene-set-project-directory (dir)
+  (setq graphene-project-root dir)
+  (setq graphene-project-folder (concat graphene-projects-folder (project-name (project-current))))
+  (unless (file-exists-p graphene-project-folder)
+    (make-directory graphene-project-folder))
+  (setq project-proj-files-dir graphene-project-folder)
+  (message (concat "Set graphene-project-root to " graphene-project-root))
+  (setq default-directory dir)
+  (message "Updating speedbar")
+  (sr-speedbar-open)
+  (speedbar-update-contents)
+  )
 
 ;; Create hooks in project-mode.
 (defadvice project-new (after graphene-project-run-after-new-hook () activate)
@@ -46,15 +62,6 @@
     (run-hooks 'graphene-project-after-save-hook))
   )
 
-;; Set graphene-project-root-dir, set default-directory, update speedbar
-(defun graphene-set-project-directory (dir)
-  (setq graphene-project-root-dir dir)
-  (message (concat "Set graphene-project-root-dir to " graphene-project-root-dir))
-  (setq default-directory dir)
-  (message "Updating speedbar")
-  (speedbar-update-contents)
-  )
-
 ;; Save any open project and kill all file-based buffers before opening a new project.
 (add-hook 'graphene-project-after-new-hook
           (lambda ()
@@ -68,7 +75,7 @@
 (add-hook 'graphene-project-before-open-hook
           (lambda ()
             (progn
-              (if (boundp 'graphene-project-root-dir)
+              (if (boundp 'graphene-project-root)
                   (project-save))
               (message "Saved project")
               (kill-all-buffers)
@@ -79,14 +86,14 @@
             (progn
               (graphene-set-project-directory
                     (project-default-directory (project-current)))
-              (message (concat "Loading project desktop from " graphene-project-root-dir))
+              (message (concat "Loading project desktop from " graphene-project-root))
               (desktop-read)
               )))
 ;; Save the project desktop.
-(add-hook 'project-after-save-hook
+(add-hook 'graphene-project-after-save-hook
           (lambda ()
             (progn
-              (message (format "Saving project desktop in %s" graphene-project-root-dir))
-              (desktop-save graphene-project-root-dir)
+              (message (format "Saving project desktop in %s" graphene-project-folder))
+              (desktop-save graphene-project-folder)
               )))
 (provide 'graphene-projects)
