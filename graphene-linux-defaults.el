033341d214d42e328b90cd9eab448f7b734bdb30
Robert Dallas Gray <mail@robertdallasgray.com>
2012-09-06 09:04:37 +0100

    Cleaned graphene-linux-defaults.

--- graphene-linux-defaults.el
+++ graphene-linux-defaults.el
@@ -1,5 +1,2 @@
-;(defvar graphene-default-font "Menlo-12")
-;(defvar graphene-fixed-pitch-family "Menlo")
-;(defvar graphene-variable-pitch-family "Lucida Sans")
 
 (provide 'graphene-linux-defaults)
