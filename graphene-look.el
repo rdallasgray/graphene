06313385f087ee77ce19f45a4a98aaab9340b50d
Robert Dallas Gray <mail@robertdallasgray.com>
2012-09-06 09:11:37 +0100

    Removed default fonts and theme.

--- graphene-look.el
+++ graphene-look.el
@@ -12,11 +12,6 @@
 ;; Soft-wrap lines
 (visual-line-mode 1)
 
-;; Default fonts
-(set-default-font graphene-default-font)
-(set-face-attribute 'variable-pitch nil :family graphene-variable-pitch-family)
-(set-face-attribute 'fixed-pitch nil :family graphene-fixed-pitch-family)
-
 ;; In GUI mode, use a bigger frame, larger line spacing, and sr-speedbar
 (if window-system
     (progn
@@ -25,12 +20,9 @@
       (add-to-list 'default-frame-alist '(line-spacing . 2))
       ;; Seems to fix graphical glitches with linum
       (set-fringe-mode '(8 . 0))
-      (load-theme 'solarized-light t)
       (sr-speedbar-open))
-  (progn
-    ;; Dark theme and menu bar off in text mode
-    (load-theme 'solarized-dark t)
-    (menu-bar-mode -1)))
+    ;; Menu bar off in text mode
+  (menu-bar-mode -1))
 
 ;; Load theme extensions
 (load-theme 'graphene t)
