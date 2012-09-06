8b4bc51389136a313f1ec25629557a995b0b9edb
Robert Dallas Gray <mail@robertdallasgray.com>
2012-09-06 08:58:54 +0100

    Moved requires to top (graphene-editing).

--- graphene-editing.el
+++ graphene-editing.el
@@ -1,15 +1,24 @@
 ;; Defaults for text editing.
 
-;; Use flymake, autocomplete, linum in prog modes
-(add-hook 'prog-mode-hook (lambda () (progn
-                                       (flymake-mode t)
-                                       (auto-complete)
-                                       (linum-mode 1)
-                                       (setq linum-format " %4d ")
-                                       )))
+(require 'smart-tab)
 (require 'auto-complete)
 (require 'auto-complete-config)
+(require 'autopair)
+(require 'multi-web-mode)
+
+;; Use flymake, autocomplete, linum in prog modes
 (ac-config-default)
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (progn
+              (flymake-mode t)
+              (auto-complete)
+              (linum-mode 1)
+              (setq linum-format " %4d ")
+              )))
+
+;; Autopair gives closer-to-textmate functionality than the built-in electric modes.
+(autopair-global-mode)
 
 ;; Nicer scrolling with mouse wheel/trackpad.
 (defun scroll-up-amount (amt) "Scroll up by the given amount."  (scroll-up-command amt))
@@ -21,22 +30,16 @@
 (global-set-key [triple-wheel-down] (lambda () (interactive) (scroll-up-amount 4)))
 (global-set-key [triple-wheel-up] (lambda () (interactive) (scroll-down-amount 4)))
 
-;; Autopair gives closer-to-textmate functionality than the built-in electric modes.
-(require 'autopair)
-(autopair-global-mode)
-
 ;; Autorevert all buffers.
 (global-auto-revert-mode t)
 
 ;; cua-mode -- only for rectangles, and to have something like delete-selection-mode that's compatible with autopair.
-(setq cua-enable-cua-keys nil)
-(cua-mode t)
+(cua-selection-mode t)
 
 ;; Always autoindent new lines.
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
 ;; Use tab for autocomplete.
-(require 'smart-tab)
 (global-smart-tab-mode 1)
 
 ;; Show matching parens immediately.
@@ -58,7 +61,6 @@
 (global-font-lock-mode t)
 
 ;; Use multi-web-mode for editing code embedded in HTML.
-(require 'multi-web-mode)
 (setq mweb-default-major-mode 'html-mode)
 (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
                   (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
