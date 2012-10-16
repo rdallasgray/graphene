9cce7a20cc7d0e1435ed57e70d34e4890a33d90e
Robert Dallas Gray <mail@robertdallasgray.com>
2012-10-16 10:43:53 +0100

    Added .pkg file.

---
+++ graphene-pkg.el
@@ -0,0 +1,11 @@
+(define-package "graphene" "20120919.2056"
+  "Newbie-friendly defaults [source: github]"
+  '((smart-tab "20120409")
+    (auto-complete "20120909")
+    (autopair "20120818")
+    (multi-web-mode "20110821")
+    (smex "20120831")
+    (sr-speedbar "20090723")
+    (project-persist "20120921")
+    (projectile "20120612")
+    (exec-path-from-shell "20121007")))
