9cda04e1987898c838f0e7373606b0c7399fa146
Robert Dallas Gray <mail@robertdallasgray.com>
2012-10-20 12:49:41 +0100

    Override stale desktop locks.

--- graphene-projects.el
+++ graphene-projects.el
@@ -81,4 +81,23 @@
             (message (format "Saving project desktop in %s" project-persist-current-project-settings-dir))
             (desktop-save project-persist-current-project-settings-dir)))
 
+;; http://www.emacswiki.org/DeskTop#toc4: Overriding stale desktop locks
+;;; desktop-override-stale-locks.el begins here
+(defun emacs-process-p (pid)
+  "If pid is the process ID of an emacs process, return t, else nil.
+Also returns nil if pid is nil."
+  (when pid
+    (let ((attributes (process-attributes pid)) (cmd))
+      (dolist (attr attributes)
+        (if (string= "comm" (car attr))
+            (setq cmd (cdr attr))))
+      (if (and cmd (or (string= "emacs" cmd) (string= "emacs.exe" cmd))) t))))
+
+(defadvice desktop-owner (after pry-from-cold-dead-hands activate)
+  "Don't allow dead emacsen to own the desktop file."
+  (when (not (emacs-process-p ad-return-value))
+    (setq ad-return-value nil)))
+;;; desktop-override-stale-locks.el ends here
+
+
 (provide 'graphene-projects)
