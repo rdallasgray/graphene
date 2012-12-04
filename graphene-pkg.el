94d971e91237c02ac345dc8182f00067030361c5
Robert Dallas Gray <robert.dallas.gray@huzutech.com>
2012-12-04 09:09:56 +0000

    Added smartparens to pkg file.

--- graphene-pkg.el
+++ graphene-pkg.el
@@ -2,7 +2,7 @@
   "Newbie-friendly defaults [source: github]"
   '((auto-complete "20120909")
     (ac-dabbrev "20091116")
-    (autopair "20120818")
+    (smartparens "20121202")
     (multi-web-mode "20110821")
     (smex "20120831")
     (sr-speedbar "20090723")
