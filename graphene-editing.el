e97ef191d0c5a1289bce3f7c14dc29eb62598c6b
Robert Dallas Gray <mail@robertdallasgray.com>
2012-12-09 09:59:06 +0000

    Added rules for smartparens in elisp.

--- graphene-editing.el
+++ graphene-editing.el
@@ -64,6 +64,10 @@
 
 ;; less annoying smartparens
 (sp-add-ban-insert-pair-in-string "'")
+(sp-add-local-ban-insert-pair "'" 'emacs-lisp-mode)
+(sp-add-local-ban-insert-pair "'" 'inferior-emacs-lisp-mode)
+(sp-add-local-ban-insert-pair-in-code "`" 'emacs-lisp-mode)
+(sp-add-local-ban-insert-pair-in-code "`" 'inferior-emacs-lisp-mode)
 
 ;; Use multi-web-mode for editing code embedded in HTML.
 (setq mweb-default-major-mode 'html-mode)
