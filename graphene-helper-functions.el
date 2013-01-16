3a6565e2fcbbd0daf3244246bd2bb62b1abc173e
Robert Dallas Gray <robert.dallas.gray@huzutech.com>
2013-01-16 09:11:04 +0000

    Fixed up cherry-pick.

--- graphene-helper-functions.el
+++ graphene-helper-functions.el
@@ -71,6 +71,26 @@
   (end-of-line)
   (newline-and-indent))
 
+(defun increase-window-height (&optional arg)
+  "Make the window taller by one line. Useful when bound to a repeatable key combination."
+  (interactive "p")
+  (enlarge-window arg))
+
+(defun decrease-window-height (&optional arg)
+  "Make the window shorter by one line. Useful when bound to a repeatable key combination."
+  (interactive "p")
+  (enlarge-window (- 0 arg)))
+
+(defun decrease-window-width (&optional arg)
+  "Make the window narrower by one line. Useful when bound to a repeatable key combination."
+  (interactive "p")
+  (enlarge-window (- 0 arg) t))
+
+(defun increase-window-width (&optional arg)
+  "Make the window shorter by one line. Useful when bound to a repeatable key combination."
+  (interactive "p")
+  (enlarge-window arg t))
+
 ;; Create a new instance of emacs
 (when window-system
   (defun new-emacs-instance ()
