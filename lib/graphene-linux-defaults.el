1e64aa7de7fa9ba39472f5b96e2c1a8d8ee82478
Robert Dallas Gray <mail@robertdallasgray.com>
2013-04-22 16:04:33 +0100

    0.1.20

--- lib/graphene-linux-defaults.el
+++ lib/graphene-linux-defaults.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Robert Dallas Gray <mail@robertdallasgray.com>
 ;; URL: https://github.com/rdallasgray/graphene
-;; Version: 0.1.19
+;; Version: 0.1.20
 ;; Keywords: defaults
 
 ;; This file is not part of GNU Emacs.
