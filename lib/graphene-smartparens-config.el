c03c2245ece7dda208d1c0a506c1e93ec9939a10
Robert Dallas Gray <mail@robertdallasgray.com>
2013-08-10 13:56:56 +0100

    0.3.2

--- lib/graphene-smartparens-config.el
+++ lib/graphene-smartparens-config.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Robert Dallas Gray <mail@robertdallasgray.com>
 ;; URL: https://github.com/rdallasgray/graphene
-;; Version: 0.3.0
+;; Version: 0.3.2
 ;; Keywords: defaults
 ;;
 ;; This file is not part of GNU Emacs.
@@ -56,7 +56,7 @@
   (require 'smartparens-ruby))
 
 ;; Markdown
-(sp-local-pair 'markdown-mode "*" "*"
+(sp-local-pair '(markdown-mode gfm-mode) "*" "*"
                :unless '(sp-in-string-p)
                :actions '(insert wrap))
 
@@ -66,5 +66,8 @@
 ;; Don't need quotes to pair following words
 (sp-pair "\"" nil :unless '(sp-point-after-word-p))
 (sp-pair "'" nil :unless '(sp-point-after-word-p))
-
+;; Except in HTML
+(sp-local-pair 'html-mode nil :unless '(:rem sp-point-after-word-p))
+;; CoffeeScript PyStrings
+(push 'coffee-mode sp-autoescape-string-quote-if-empty)
 (provide 'graphene-smartparens-config)
