8d35ec5d97ad9673e0c363b415b70f499d6bf2ef
Robert Dallas Gray <mail@robertdallasgray.com>
2013-08-21 20:16:59 +0100

    0.3.6

--- lib/graphene-env.el
+++ lib/graphene-env.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Robert Dallas Gray <mail@robertdallasgray.com>
 ;; URL: https://github.com/rdallasgray/graphene
-;; Version: 0.3.5
+;; Version: 0.3.6
 ;; Keywords: defaults
 
 ;; This file is not part of GNU Emacs.
