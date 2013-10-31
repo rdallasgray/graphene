c29f63df8c84cb93fc492730d0406fcfe4ae8ea1
Robert Dallas Gray <robert@geo.me>
2013-10-31 09:31:53 +0000

    Rebuild

--- lib/graphene-speedbar.el
+++ lib/graphene-speedbar.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Robert Dallas Gray <mail@robertdallasgray.com>
 ;; URL: https://github.com/rdallasgray/graphene
-;; Version: 0.4.0
+;; Version: 0.4.1
 ;; Keywords: defaults
 
 ;; This file is not part of GNU Emacs.
