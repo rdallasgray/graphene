af8cab325a6a925611608f7546cec9b827edf774
Robert Dallas Gray <mail@robertdallasgray.com>
2013-11-17 15:50:37 +0000

    Amend function names

--- graphene-smartparens-config.el
+++ graphene-smartparens-config.el
@@ -33,23 +33,23 @@
 
 ;;; Code:
 
-(defun gp/sp/pair-on-newline (id action context)
+(defun graphene--sp-pair-on-newline (id action context)
   "Put trailing pair on newline and return to point."
   (save-excursion
     (newline)
     (indent-according-to-mode)))
 
-(defun gp/sp/pair-on-newline-and-indent (id action context)
+(defun graphene--sp-pair-on-newline-and-indent (id action context)
   "Open a new brace or bracket expression, with relevant newlines and indent. "
-  (gp/sp/pair-on-newline id action context)
+  (graphene--sp-pair-on-newline id action context)
   (indent-according-to-mode))
 
 (sp-pair "{" nil :post-handlers
          '(:add ((lambda (id action context)
-                   (gp/sp/pair-on-newline-and-indent id action context)) "RET")))
+                   (graphene--sp-pair-on-newline-and-indent id action context)) "RET")))
 (sp-pair "[" nil :post-handlers
          '(:add ((lambda (id action context)
-                   (gp/sp/pair-on-newline-and-indent id action context)) "RET")))
+                   (graphene--sp-pair-on-newline-and-indent id action context)) "RET")))
 
 ;; Markdown
 (sp-local-pair '(markdown-mode gfm-mode) "*" "*"
